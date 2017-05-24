bd5fc47bc7dd8fe269bb3dc61b57ff3164d5214a
Ryan Mahler <Ryan.Mahler@D2L.com>
2017-05-24 17:48:14 -0400

    Add built-in skeleton and tests
    And... probably some other changes. Oh, updated the README and demo a bit too.

---
+++ test/d2l-user-tile-auto/d2l-user-tile-auto.js
@@ -0,0 +1,89 @@
+/* global describe, beforeEach, afterEach, it, expect, fixture, sinon */
+
+'use strict';
+
+describe('<d2l-user-tile-auto>', function() {
+	var component,
+		sandbox,
+		userUrl = 'https://example.com',
+		token = 'some-oauth-token';
+
+	beforeEach(function() {
+		component = fixture('basic');
+		sandbox = sinon.sandbox.create();
+	});
+
+	afterEach(function() {
+		sandbox.restore();
+	});
+
+	it('is instantiated', function() {
+		expect(component).to.exist;
+	});
+
+	describe('fetching user data', function() {
+		it('should not generate the user request if the URL is empty', function(done) {
+			var spy = sandbox.spy(component, '_onUserChange');
+			var stub = sandbox.stub(component, 'generateUserRequest');
+			component.token = token;
+			setTimeout(function() {
+				expect(spy.called).to.be.true;
+				expect(stub.called).to.be.false;
+				done();
+			});
+		});
+
+		it('should not generate the user request if the token is empty', function(done) {
+			var spy = sandbox.spy(component, '_onUserChange');
+			var stub = sandbox.stub(component, 'generateUserRequest');
+			component.userUrl = userUrl;
+			setTimeout(function() {
+				expect(spy.called).to.be.true;
+				expect(stub.called).to.be.false;
+				done();
+			});
+		});
+
+		it('should generate the user request when both token and URL are set', function(done) {
+			var spy = sandbox.spy(component, '_onUserChange');
+			var stub = sandbox.stub(component, 'generateUserRequest');
+			component.token = token;
+			component.userUrl = userUrl;
+			setTimeout(function() {
+				expect(spy.called).to.be.true;
+				expect(stub.calledWith(userUrl, token, sinon.match({ background: true }))).to.be.true;
+				done();
+			});
+		});
+
+		it('sets the properties on the internal <d2l-user-tile> appropriately', function(done) {
+			sandbox.stub(component, 'generateUserRequest', function() {
+				component._name = 'name';
+				component._school = 'school';
+				component._iconUrl = 'iconUrl';
+				component._backgroundUrl = 'backgroundUrl';
+				component._backgroundColor = 'backgroundColor';
+			});
+
+			component.token = token;
+			component.userUrl = userUrl;
+			setTimeout(function() {
+				var innerTile = component.$$('d2l-user-tile');
+				expect(innerTile.name).to.equal('name');
+				expect(innerTile.school).to.equal('school');
+				expect(innerTile.icon).to.equal('iconUrl');
+				expect(innerTile.background).to.equal('backgroundUrl');
+				expect(innerTile.backgroundColor).to.equal('backgroundColor');
+				done();
+			});
+		});
+	});
+
+	describe('content placeholders', function() {
+		it('should set the placeholder property on the internal <d2l-user-tile>', function() {
+			component = fixture('with-placeholders');
+			var internalTile = component.$$('d2l-user-tile');
+			expect(internalTile.placeholders).to.be.true;
+		});
+	});
+});
--- test/d2l-user-tile/d2l-user-tile.js
+++ test/d2l-user-tile/d2l-user-tile.js
@@ -17,7 +17,6 @@ describe('<d2l-user-tile>', function() {
 	});
 
 	describe('given that the tile should render the appropriate data provided', function() {
-
 		describe('when the `icon` attribute is not provided', function() {
 			it('should render the default icon only', function() {
 				expect(component.querySelector('d2l-icon')).to.exist;
@@ -34,6 +33,54 @@ describe('<d2l-user-tile>', function() {
 			it('should render the custom icon only', function() {
 				expect(component.querySelector('d2l-icon')).to.not.exist;
 				expect(component.querySelector('d2l-image')).to.exist;
+			});
+		});
+	});
+
+	describe('content placeholders', function() {
+		beforeEach(function() {
+			component = fixture('with-placeholders');
+		});
+
+		describe('for name field', function() {
+			it('should show a placeholder for the name when name is not set', function() {
+				var name = component.$$('.user-tile-name:not(.text-placeholder)');
+				var placeholder = component.$$('.user-tile-name.text-placeholder');
+				expect(name.getAttribute('hidden')).to.equal('');
+				expect(placeholder.getAttribute('hidden')).to.be.null;
+			});
+
+			it('should hide the placeholder for the name when name is set', function() {
+				component.name = '';
+				var name = component.$$('.user-tile-name:not(.text-placeholder)');
+				var placeholder = component.$$('.user-tile-name.text-placeholder');
+				expect(name.getAttribute('hidden')).to.be.null;
+				expect(placeholder.getAttribute('hidden')).to.equal('');
+			});
+		});
+
+		describe('for school/grade field', function() {
+			it('should show a placeholder when neither grade nor school is set', function() {
+				var text = component.$$('.user-tile-school:not(.text-placeholder)');
+				var placeholder = component.$$('.user-tile-school.text-placeholder');
+				expect(text.getAttribute('hidden')).to.equal('');
+				expect(placeholder.getAttribute('hidden')).to.be.null;
+			});
+
+			it('should hide the placeholder when school is set', function() {
+				component.school = '';
+				var text = component.$$('.user-tile-school:not(.text-placeholder)');
+				var placeholder = component.$$('.user-tile-school.text-placeholder');
+				expect(text.getAttribute('hidden')).to.be.null;
+				expect(placeholder.getAttribute('hidden')).to.equal('');
+			});
+
+			it('should hide the placeholder when grade is set', function() {
+				component.grade = '';
+				var text = component.$$('.user-tile-school:not(.text-placeholder)');
+				var placeholder = component.$$('.user-tile-school.text-placeholder');
+				expect(text.getAttribute('hidden')).to.be.null;
+				expect(placeholder.getAttribute('hidden')).to.equal('');
 			});
 		});
 	});
