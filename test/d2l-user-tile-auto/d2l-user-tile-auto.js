d61f8c836d673107dde990d4ad3174ad9e164a98
jstefaniuk <jstefaniuk@D2L.com>
2017-06-16 01:25:56 -0500

    why do these tests not fail locally at allll

--- test/d2l-user-tile-auto/d2l-user-tile-auto.js
+++ test/d2l-user-tile-auto/d2l-user-tile-auto.js
@@ -57,6 +57,8 @@ describe('<d2l-user-tile-auto>', function() {
 		});
 
 		it('sets the properties on the internal <d2l-user-tile> appropriately', function(done) {
+			var innerTile = component.$$('d2l-user-tile');
+			sandbox.stub(innerTile, '_onImageLoadFailure', function() {});
 			sandbox.stub(component, 'generateUserRequest', function() {
 				component._name = 'name';
 				component._iconUrl = 'iconUrl';
@@ -67,7 +69,6 @@ describe('<d2l-user-tile-auto>', function() {
 			component.token = token;
 			component.userUrl = userUrl;
 			setTimeout(function() {
-				var innerTile = component.$$('d2l-user-tile');
 				expect(innerTile.name).to.equal('name');
 				expect(innerTile.icon).to.equal('iconUrl');
 				expect(innerTile.background).to.equal('backgroundUrl');
